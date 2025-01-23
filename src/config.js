15550f60dbfdd7c7e620e4a981b577f484cde7b1
Krissuarez <88417369+Krissuarez@users.noreply.github.com>
2025-01-23 15:54:01 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -11,11 +11,11 @@ exports.BOT_NAME = "Takeshi Bot";
 
 // Número do bot. Coloque o número do bot
 // (apenas números, exatamente como está no WhatsApp).
-exports.BOT_NUMBER = "559999999999";
+exports.BOT_NUMBER = "593984414308";
 
 // Número do dono do bot. Coloque o número do dono do bot
 // (apenas números, exatamente como está no WhatsApp).
-exports.OWNER_NUMBER = "559999999999";
+exports.OWNER_NUMBER = "593984414308";
 
 // Diretório dos comandos
 exports.COMMANDS_DIR = path.join(__dirname, "commands");
